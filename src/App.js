e328aa9366ede8d07fcb0f82edef62c3e97815c1
vadiminsk <vadim@graphicalliance.co.uk>
2021-03-01 21:45:39 +0300

    add useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => {
+const App = ({ initialCount }) => {
+  let [count, setCount] = useState(initialCount);
+
+  const addOne = () => {
+    setCount(count + 1);
+  };
+
+  const minusOne = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
   return (
     <div>
-      <h3>Count : 0</h3>
+      <h3>Count : {count}</h3>
+      <button onClick={addOne}>Add one</button>
+      <button onClick={minusOne}>Minus one</button>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App initialCount={0} />
   </React.StrictMode>,
   document.getElementById('root')
 );
